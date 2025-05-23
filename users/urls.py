dac9ad90c89ed102a1eb4554e6949b0dca4cf00e
Gabriel2146 <marcelo.padilla.rodriguez@udla.edu.ec>
2025-05-23 17:33:32 -0500

    dashboard admon, etc

--- users/urls.py
+++ users/urls.py
@@ -16,4 +16,11 @@ urlpatterns = [
     path('profile-api/', UserProfileView.as_view(), name='user-profile'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    path('admin/exercises/', simple_views.admin_exercises, name='admin_exercises'),
+    path('admin/training-plans/', simple_views.admin_training_plans, name='admin_training_plans'),
+    path('trainer/training-plans/', simple_views.trainer_training_plans, name='trainer_training_plans'),
+    path('athlete/training-plans/', simple_views.athlete_training_plans, name='athlete_training_plans'),
+    path('guest/content/', simple_views.guest_content, name='guest_content'),
+
 ]
