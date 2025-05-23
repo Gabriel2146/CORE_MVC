66f32452b42f2469ba039c38ba8ff354fa77e5ee
Gabriel2146 <marcelo.padilla.rodriguez@udla.edu.ec>
2025-05-23 16:28:22 -0500

    funcionalidades por rol

--- users/simple_views.py
+++ users/simple_views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .forms import CustomUserCreationForm
+from training import models as training_models
+from django.views.decorators.http import require_http_methods
+from django.core.exceptions import PermissionDenied
 
 def user_login(request):
     if request.method == 'POST':
@@ -43,3 +46,43 @@ def dashboard(request):
         return render(request, 'users/dashboard_athlete.html', {'user': user})
     else:
         return render(request, 'users/dashboard_guest.html', {'user': user})
+
+@login_required
+@require_http_methods(["GET"])
+def admin_exercises(request):
+    if request.user.role != 'admin':
+        raise PermissionDenied
+    exercises = training_models.Exercise.objects.all()
+    return render(request, 'users/admin_exercises.html', {'exercises': exercises})
+
+@login_required
+@require_http_methods(["GET"])
+def admin_training_plans(request):
+    if request.user.role != 'admin':
+        raise PermissionDenied
+    plans = training_models.TrainingPlan.objects.all()
+    return render(request, 'users/admin_training_plans.html', {'plans': plans})
+
+@login_required
+@require_http_methods(["GET"])
+def trainer_training_plans(request):
+    if request.user.role != 'trainer':
+        raise PermissionDenied
+    plans = training_models.TrainingPlan.objects.filter(user=request.user)
+    return render(request, 'users/trainer_training_plans.html', {'plans': plans})
+
+@login_required
+@require_http_methods(["GET"])
+def athlete_training_plans(request):
+    if request.user.role != 'athlete':
+        raise PermissionDenied
+    plans = training_models.TrainingPlan.objects.filter(user=request.user)
+    return render(request, 'users/athlete_training_plans.html', {'plans': plans})
+
+@login_required
+@require_http_methods(["GET"])
+def guest_content(request):
+    if request.user.role != 'guest':
+        raise PermissionDenied
+    exercises = training_models.Exercise.objects.all()
+    return render(request, 'users/guest_content.html', {'exercises': exercises})
